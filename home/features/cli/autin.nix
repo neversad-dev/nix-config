4cb18f9f0fa029c2eb95c2608fffdb49819b39de
neversad-dev <7419136+neversad-dev@users.noreply.github.com>
2026-09-23 19:38:44 +0300

    feat: change atuin package to unstable

--- home/features/cli/autin.nix
+++ home/features/cli/autin.nix
@@ -1,8 +1,9 @@
 # Atuin: searchable shell history (SQLite); optional encrypted sync
-{...}: {
+{pkgs-unstable, ...}: {
   programs = {
     atuin = {
       enable = true;
+      package = pkgs-unstable.atuin;
       settings = {
         enter_accept = true;
       };
